81b798b5b97ee241c6832d1e6beb2cf3fa6df6a1
David Robinson <drobinson@mz.com>
2018-05-15 21:41:49 -0700

    Tweaked Call Routes for Testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,7 @@
 import chalk from 'chalk';
 import path from 'path';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 export default function (props) {
   const { app } = props;
@@ -19,6 +20,7 @@ export default function (props) {
   // app.set('view engine', 'pug');
   // app.set('views', path.join(__dirname, '../views'));
   // app.use(express.static(path.join(__dirname, '../../static')));
+  app.use(bodyParser.urlencoded({ extended: false }));
 
   app.get('/', (req, res) => {
     res.send('<h1>AUTOCAT IS RUNNING RIGHT MEOW</h1>');
@@ -26,21 +28,26 @@ export default function (props) {
 
   app.post('/message', (req, res) => {
     console.log('SMS Recieved');
-    console.log(res);
+    console.log(req);
   });
 
   app.post('/message-fail', (req, res) => {
     console.log('SMS Failed');
-    console.log(res);
+    console.log(req);
   });
 
   app.post('/call', (req, res) => {
     console.log('Call Recieved');
-    console.log(res);
+    console.log(req);
   });
 
   app.post('/call-fail', (req, res) => {
     console.log('Call Failed');
-    console.log(res);
+    console.log(req);
+  });
+
+  app.post('/call-status', (req, res) => {
+    console.log('Call Status Changed');
+    console.log(req);
   });
 }
